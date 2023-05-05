14f1112dc38b4932da49b106f1cb82475e0f4533
fernanda rodriguez <fernandarpy@gmail.com>
2023-05-05 15:33:05 -0500

    style meaning

--- src/components/ContainMeaning/ContainMeaning.jsx
+++ src/components/ContainMeaning/ContainMeaning.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./containMeaning.scss"
 import TitleMeaning from "../TitleMeaning/TitleMeaning";
+import DescriptionMeaning from "../DescriptionMeaning/DescriptionMeaning"
 
 const ContainMeaning = ()=>{
     return(
         <div className="containMeaning">
 <TitleMeaning/>
+<DescriptionMeaning/>
         </div>
     )
 }
---
+++ src/components/DescriptionMeaning/DescriptionMeaning.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./descriptionMeaning.scss"
+
+const DescriptionMeaning = () => {
+    return(
+<div>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At nemo sint corporis temporibus, cupiditate, consectetur tenetur eaque aliquid itaque eum autem vitae laboriosam laudantium magnam. Amet accusantium ipsam adipisci minima.</p>
+</div>
+    )
+}
+
+export default DescriptionMeaning;
